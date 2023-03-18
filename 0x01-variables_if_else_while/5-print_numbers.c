9a9512c73f61a19f16bc5d7aeda0adfdf958eacc
Lhalharitgihub <aziwemputa@gmail.com>
2023-03-18 10:21:54 -0700

    C - Variables, if, else, while Hot

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
-{       
-        int i;
-        
-        for (i = 0; i < 10; i++)
-        {       
-                putchar("%d", i);
-        }       
-        putchar('\n');
-        return (0);
-}       
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
