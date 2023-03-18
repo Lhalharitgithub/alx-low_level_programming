c9d0e4e73696a529db14eb0e545b15f40881f61e
Lhalharitgihub <aziwemputa@gmail.com>
2023-03-18 09:49:43 -0700

    C - Variables, if, else, while Hot

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
+{
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
-        
-        for (i = 0; i < 52; i++)
-        {       
-                putchar(alp[i]);
-        }       
-        putchar('\n');
-        return (0);
-}       
+	int i;
+	
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
