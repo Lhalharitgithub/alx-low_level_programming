e94ddd2065423938fab6ca492c889b1c7bef173e
Lhalharitgihub <aziwemputa@gmail.com>
2023-03-18 11:38:25 -0700

    C - Variables, if, else, while HOT

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        int n, m, l;
-        
-        for (n = 48; n < 58; n++)
-        {    
-                for (m = 49; m < 58; m++)
-                {
+{
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
 			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
@@ -24,10 +24,10 @@ int main(void)
 						putchar(',');
 						putchar(' ');
 					}
-                                }
-                        }
-                }
-        }       
-        putchar('\n');
-        return (0);
-}       
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
