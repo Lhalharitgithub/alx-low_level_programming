dceb5ab393dd279fce9c28e100304d66ff84a03b
Lhalharitgihub <aziwemputa@gmail.com>
2023-03-18 12:11:10 -0700

    C - Variables, if, else, while HOT

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,13 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        char ch;
-        
-        for (ch = 'z'; ch >= 'a'; ch--)
-        {       
-                putchar(ch);
-        }       
-        putchar('\n');
-        return (0);
-}       
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
